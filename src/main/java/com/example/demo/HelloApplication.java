5a1907d6945050f5346ed44d7ce2c6696fd1b30f
parchano <chano22@naver.com>
2020-11-03 00:51:13 +0900

    test push

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -14,6 +14,6 @@ public class HelloApplication {
 	}
 	@GetMapping(value = "/hello")
 	public String hello() {
-		return "hello!!";
+		return "hello!!sdalkfjsdalkfjsdalk;fjsadl;kfjlsa;fdljsk";
 	}
 }
